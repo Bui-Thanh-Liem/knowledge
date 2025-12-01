87bcc54b1f9c004e3209a64ab44e6ede2fcd253c
liemdev <buithanhliem@5073.com>
2025-12-01 10:48:41 +0700

    update code

--- tech/redis/transaction/cli.js
+++ tech/redis/transaction/cli.js
@@ -6,4 +6,14 @@
  * discard => hủy transaction
  *
  * watch key [key ...] => theo dõi các key, nếu có thay đổi sẽ hủy transaction
+ *
+ * unwatch => hủy theo dõi các key
+ *
+ * - khi watch một giá trị, nếu giá trị bị thay đổi trước khi exec thì transaction sẽ bị huỷ
+ *   không thực thi bất cứ lệnh nào
+ *
+ * - Nếu có lệnh sai cú pháp thì các lệnh khác sẽ không thực thi
+ *
+ * - Nếu sai do code (incr string) thì các lệnh khác vẫn thực thi bình thường
+ *
  */
